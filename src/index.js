2263dfd49ded6b3c6be2f6cb5396b7a6bac75d2a
GabGueAlva <agabrielaguevaraa@hotmail.com>
2024-07-04 16:35:58 -0500

    A

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require("cors")
+
+const app = express()
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors())
+
+//route
+app.use(require('./routes/index'))
+
+
+app.listen(4000)
+console.log('Server on port 4000')
